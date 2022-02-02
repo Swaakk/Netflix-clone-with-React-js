ce42adc10a72401276beecedcef29704bba48c72
Swaakk <swathykrishna.thayyil@gmail.com>
2022-02-02 13:53:57 +0530

    going to finishing stage

--- src/component/Banner/Banner,.js
+++ src/component/Banner/Banner,.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {API_KEY} from "../../constants/constants"
+import axios from '../axios';
 import "./Banner.css"
-
+ 
 
 function Banner(){
+    const [movie, setMovie] = useState(1);
+    const [description,setDescription] = useState(1);
+    useEffect(()=>{
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((Response)=>{
+            console.log(Response.data.results[1]);
+            setMovie(Response.data.results[1])
+        })
+    }, [])
+
+    useEffect(()=>{
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((Response)=>{
+            console.log(Response.data.results[1]);
+            setDescription(Response.data.results[1])
+        })
+    },[])
     return(
         <div className='banner'>
             <div className='content'>
-                <h1 className='title'>Movie Name</h1>
+                <h1 className='title'>{movie.title}</h1>
                 <div className='banner-buttons'>    
                     <button className='button'>Play</button>
                     <button className='button'>My list</button>
                 </div>
                 <h1 className='description'>
-                   cilis consequatur itaque officia necessitatibus iure totam omnis, ratione saepe corporis voluptas. Temporibus esse vitae dolores quod! Rem sed officia adipisci fuga voluptatem porro nulla nemo ipsum eum?
+                    {description.overview}
+                   {/* cilis consequatur itaque officia necessitatibus iure totam omnis, ratione saepe corporis voluptas. Temporibus esse vitae dolores quod! Rem sed officia adipisci fuga voluptatem porro nulla nemo ipsum eum? */}
                 </h1>
             </div>
             <div className="fade-bottom"></div>
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = "cac03f21b07b61f4ff76136e66195416"
